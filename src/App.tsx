fb8447e60febff0a308535cd2e95a4b1871d45c6
Sam Fentress <sfentress@concord.org>
2019-03-05 12:49:35 -0500

    Initial basic form

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,57 @@ import React, { Component } from 'react';
 import { initializePlugin, openTable, addData, createDataContext } from './lib/codap-helper';
 import './App.css';
 
-const kPluginName = "Sample Plugin";
+const kPluginName = "Collaborative Data Sharing";
 const kVersion = "0.0.1";
 const kInitialDimensions = {
-  width: 200,
-  height: 200
+  width: 350,
+  height: 400
 }
-const kDataContextName = "SamplePluginData";
+const kDataContextName = "collaborative tables";
 
 class App extends Component {
   public componentWillMount() {
-    initializePlugin(kPluginName, kVersion, kInitialDimensions)
-      .then(() => createDataContext(kDataContextName));
+    initializePlugin(kPluginName, kVersion, kInitialDimensions);
+      // .then(() => createDataContext(kDataContextName));
   }
 
   public render() {
     return (
       <div className="App">
-        Hello World
-        <div>
-          <button onClick={this.handleOpenTable}>
-            Open Table
-          </button>
-          <button onClick={this.handleCreateData}>
-            Create some data
-          </button>
-        </div>
+        To create or join a collaborative table
+        <ol>
+          <li>
+            Select a table to share <strong>or</strong> create a new one
+            <div>
+              <select value={"new-table"}>
+                <option value="new-table">Create new table</option>
+              </select>
+            </div>
+          </li>
+          <li>
+            Provide a name or label for grouping
+            <div>
+              <input type="text" value={""} />
+            </div>
+          </li>
+          <li>
+            Invite others to join you your table <strong>or</strong> join another group
+            <div>
+              <div>
+                <button>Allow others to join your table</button>
+              </div>
+              <div>
+                or
+              </div>
+              <div>
+                Enter code to join another group: <input type="text" value={""} />
+                <button>Join</button>
+              </div>
+            </div>
+          </li>
+        </ol>
       </div>
     );
-  }
-
-  private handleOpenTable() {
-    openTable();
-  }
-
-  private handleCreateData() {
-    addData(kDataContextName, [1, 2, 3])
   }
 }
 
