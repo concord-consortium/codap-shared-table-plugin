165370ed52caf4a05dda0a6bfeb376108a429fc6
Sam Fentress <sfentress@concord.org>
2019-03-15 13:29:22 -0400

    Fix export and typos

--- firebase-functions/functions/src/index.ts
+++ firebase-functions/functions/src/index.ts
@@ -12,9 +12,9 @@ admin.initializeApp();
 
 /**
  * This database triggered function will delete /shared-tables/{shareId} when
- * /shared-tables/{shareId}/connectedUsers is deleted, which happens after the last person leaves the sahe
+ * /shared-tables/{shareId}/connectedUsers is deleted, which happens after the last person leaves the share
  */
-exports.deleteDataWithNoConnectedUsers = functions.database.ref('/shared-tables/{shareId}/connectedUsers').onDelete(async (change) => {
+export const deleteDataWithNoConnectedUsers = functions.database.ref('/shared-tables/{shareId}/connectedUsers').onDelete(async (change) => {
   const ref = change.ref.parent;
   if (ref) {
     return ref.remove();
